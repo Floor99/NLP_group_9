1639993a4b18f8d1dc1a4506bb3815e47b81859f
Floor99 <floorhalkes@gmail.com>
2024-10-20 21:32:07 +0200

    full preprocess steps

--- NLP/nlp/preprocessing_functions.py
+++ NLP/nlp/preprocessing_functions.py
@@ -1,9 +1,13 @@
+from collections import Counter
 import pandas as pd
 import re
 import spacy
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from nltk.probability import FreqDist
+from nltk.stem import WordNetLemmatizer
+
 
 nltk.download('punkt')
 nltk.download('stopwords')
@@ -55,10 +59,71 @@ def remove_stopwords(series:pd.Series, language:str= "english") -> pd.Series:
 
 # LEMMATIZE WORDS 
 def lemmatize_text(text: str) -> str:
-        doc = nlp(text)  
-        lemmatized_words = [token.lemma_ for token in doc]  
-        return ' '.join(lemmatized_words)
+    text = ' '.join(text)
+    doc = nlp(text) 
+    lemmatized_words = [token.lemma_ for token in doc]  
+    return lemmatized_words
     
 def lemmatize_words(series:pd.Series) -> pd.Series:
     series = series.copy()
     return series.apply(lemmatize_text)
+
+def lemmatize(series:pd.Series) -> pd.Series:
+    series = series.copy()
+    return series.apply(lambda x:lemmatize_txt(x))
+
+def lemmatize_txt(text:str) -> str:
+    wordnet_lemmatizer = WordNetLemmatizer()
+    lemm_text = [wordnet_lemmatizer.lemmatize(word) for word in text]
+    return lemm_text
+
+
+### Word frequency 
+def get_word_frequency(series:pd.Series):
+    all_words_after_preprocessing = [l for ls in series for l in ls]
+    counter = Counter(all_words_after_preprocessing)
+    word_count = counter.most_common()
+    return word_count
+
+def n_most_frequent_words(series:pd.Series, n_words):
+    all_words = [word for sublist in series for word in sublist]
+
+    fdist = FreqDist(all_words)
+    
+    df_fdist = pd.DataFrame({'Word': fdist.keys(),
+                             'Frequency': fdist.values()})
+    df_fdist = df_fdist.sort_values(by='Frequency', ascending=False)
+    
+    most_freq_words_list = list(df_fdist['Word'][0:n_words])
+    
+    return most_freq_words_list
+
+def multiple_word_remove_func(series:pd.Series, words_2_remove_list):    
+    cleaned_series = series.apply(
+        lambda word_list: [word for word in word_list if word not in words_2_remove_list]
+    )
+    
+    return cleaned_series
+
+def n_times_least_used(series:pd.Series, n_times):
+    all_words = [word for sublist in series for word in sublist]
+    fdist = FreqDist(all_words)
+    df_fdist = pd.DataFrame({'Word': fdist.keys(),
+                             'Frequency': fdist.values()})
+    df_fdist = df_fdist.sort_values(by='Frequency', ascending=False)
+    
+    least_used = df_fdist.loc[df_fdist['Frequency'] <= n_times]
+
+    return least_used
+
+def get_number_of_words_with_up_to_n_times_present(word_frequency, n):
+    least_common = sum(1 for t in word_frequency if 1 <= t[1] <= n)
+    return least_common
+
+
+
+
+
+
+
+
